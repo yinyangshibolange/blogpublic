6ffdca7f2a50f7499560b9ec514551b169520777
yinyangshibolange <1192492051@qq.com>
2024-06-18 17:31:56 +0800

    xzcv

--- js/jquery_dom.js
+++ js/jquery_dom.js
@@ -1,8 +1,8 @@
-!function(s){window.newConfirm=function({title:c="提示",content:o="",ok:i=()=>{},cancle:n=()=>{}}){c=`
+!function(e){window.newConfirm=function({title:i="提示",content:o="",ok:n=()=>{},cancle:c=()=>{}}){i=`
             <div class="confirm">
                 <div class="confirm-mask"></div>
                 <div class="confirm-content">
-                    <div class="confirm-title">${c}</div>
+                    <div class="confirm-title">${i}</div>
                     <div class="confirm-body">${o}
                   
                     </div>
@@ -15,8 +15,17 @@
                     </div> 
                 </div>
             </div>
-        `;s("body").append(c),s(".confirm-ok").click(function(){i(),s(".confirm").remove()}),s(".confirm-cancle").click(function(){n(),s(".confirm").remove()})},window.showMessage=function(c,o){history.replaceState(null,null,window.location.href.replace(/[\?&]success=[^&]*/,"").replace(/[\?&]message=[^&]*/,""));let i,n;"1"===c||1===c?(i="success",n=o||""):"0"!==c&&0!==c||(i="error",n=o||""),n&&(o=`
-            <div class="message ${i}">
-           ${"1"===c?'<i class="iconfont icon-checkgou"></i>':'<i class="iconfont icon-about"></i>'}   <div class="message-body">${n}</div>
+        `;e("body").append(i),e(".confirm-ok").click(function(){n(),e(".confirm").remove()}),e(".confirm-cancle").click(function(){c(),e(".confirm").remove()})},window.showMessage=function(i,o){history.replaceState(null,null,window.location.href.replace(/[\?&]success=[^&]*/,"").replace(/[\?&]message=[^&]*/,""));let n,c;"1"===i||1===i?(n="success",c=o||""):"0"!==i&&0!==i||(n="error",c=o||""),c&&(o=`
+            <div class="message ${n}">
+           ${"1"===i?'<i class="iconfont icon-checkgou"></i>':'<i class="iconfont icon-about"></i>'}   <div class="message-body">${c}</div>
             </div>
-        `,s("body").append(o),setTimeout(()=>{s(".message").remove()},2e3))}}(jQuery);
+        `,e("body").append(o),setTimeout(()=>{e(".message").remove()},2e3))},window.previewImage=function(i){i=e(`
+                        <img src="${i}" style="position: fixed; width: 100%; height: 100%; top: 0; left: 0; background : rgba(0,0,0,0.5); object-fit: contain; z-index: 90010192;">
+            
+            `).on("click",function(i){this.remove()});e("body").append(i)},window.previewVideo=function(i){i=e(`
+            <div>
+                <div class="mask" style="position: fixed; width: 100%; height: 100%; top: 0; left: 0; background : rgba(0,0,0,0.5); object-fit: contain; z-index: 90010191;"></div>
+                <video src="${i}" style="position: fixed; width: 100%; height: 100%; top: 0; left: 0; background : rgba(0,0,0,0.5); object-fit: contain; z-index: 90010192;">
+                </video>
+            </div>
+        `).on("click",function(i){this.remove()});e("body").append(i)}}(jQuery);
